c0494f0d526270ae60500aade7c2e0151b0ea0ad
Dzmitry Furmanau <amator228@gmail.com>
2019-08-02 15:50:30 +0300

    upd GDI

--- src/com/autoPartsStore/app/dao/impl/GenericDaoImpl.java
+++ src/com/autoPartsStore/app/dao/impl/GenericDaoImpl.java
@@ -9,17 +9,23 @@ import java.util.Optional;
 //TODO rename class and make it as util
 public class GenericDaoImpl<T extends BaseEntity> implements GenericDao<T> {
 
+    //private FileWriter<T> tFileWriter = new FileWriter<>();
+    //private FileReader<T> tFileReader = new FileReader<>();
+
     @Override
     //TODO generate ID autoincrement
     public Optional<T> save(String path, T object) {
         //TODO
         //1. Add read from file
+        //final Collection<T> collection = tFileReader.read(path, objectTFunction);
         //2. check if exist by name
         //3. generate ID
+        //final Optional<Long> longOptional = collection.stream().map(t -> ((BaseEntity) t).getId()).max(Comparator.naturalOrder());
+        //final long id = longOptional.orElse(0L) + 1;
         //4. add to collection
         //collection.add
         //5. write collection to file
-
+        //tFileWriter.write(path, Collections.singletonList(object));
         return Optional.ofNullable(object);
     }
 
